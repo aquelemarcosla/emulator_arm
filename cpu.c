321b2e310a6c2f5a10a7ab6e7078f35bde7a1994
Marcos Aurelio <83597143+aquelemarcosla@users.noreply.github.com>
2026-04-20 20:03:03 -0300

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -1,15 +1,15 @@
 #include "cpu.h"
 #include "stdint.h"
 
-void reg_write(CPU *cpu, int reg, uint64_t value) {
-    if (reg == 31) {
+void reg_write(CPU *cpu, int reg, uint64_t value) {  // Escrita registrador
+    if (reg == 31) { // caso XZR
         return;
     }
     cpu->regs[reg] = value;
 }
 
-uint64_t reg_read(CPU *cpu, int reg) {
-    if (reg == 31) {
+uint64_t reg_read(CPU *cpu, int reg) {  // Leitura Registrador
+    if (reg == 31) { // caso XZR
         return 0;
     }
     return cpu->regs[reg];
