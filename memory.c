528198489fb94d75224990593249c31bf69da75e
Marcos Aurelio <83597143+aquelemarcosla@users.noreply.github.com>
2026-04-23 00:20:53 -0300

    .

---
+++ memory.c
@@ -0,0 +1,15 @@
+#include "memory.h"
+#include <stdint.h>
+
+uint64_t mem_read(uint64_t address) {
+    uint64_t result = 0;
+
+    if (address > MEM_SIZE) {
+        // limite
+    }
+
+    for (int i = 0; i <= 7; i++) {
+        result |= (uint64_t)memory[address + i] << (i * 8); // cast para 64 bits antes do shift para não estourar o dado
+    }
+    return result;
+}
--- memory.h
+++ memory.h
@@ -2,13 +2,13 @@
 #define MEMORY_H
 #include <stdint.h>
 
-#define MEM_SIZE 1310770
+#define MEM_SIZE 131072
 
 uint8_t memory[MEM_SIZE];
 
 uint64_t mem_read(uint64_t address);
 
-uint64_t mem_write(uint64_t address, uint64_t data);
+void mem_write(uint64_t address, uint64_t data);
 
 #endif
 
