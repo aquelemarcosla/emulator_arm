481342d8f7c54940841aa6e617e4b3f73651ac68
Marcos <marcosaurelio060204@gmail.com>
2026-06-07 22:20:51 -0300

    Adição a tabela opcode encode

--- encode/builders/builderDPR/dpi_Shift/builderDPR_Shift.h
+++ encode/builders/builderDPR/dpi_Shift/builderDPR_Shift.h
@@ -2,7 +2,7 @@
 #define BUILDER_DPR_SHIFT_H
 #include "stdint.h"
 
-uint32_t builderDPR_LSRV(uint32_t instruction);
-uint32_t builderDPR_LSLV(uint32_t instruction);
+uint32_t builderLSRV(uint32_t instruction);
+uint32_t builderLSLV(uint32_t instruction);
 
 #endif // BUILDER_DPR_SHIFT_H
--- encode/opcode_table/opcode_table.c
+++ encode/opcode_table/opcode_table.c
@@ -13,7 +13,15 @@ OpcodeTable opcode_table[] = {
     {"MOVZ", 0x694, &builderMOVZ},
     {"MOVN", 0x494, &builderMOVN},
     {"LSL", 0x34D, &builderUBFM},
-    {"LSR", 0x34D, &builderUBFM}
+    {"LSR", 0x34D, &builderUBFM},
+    // DPR
+    {"ADD", 0x458, &builderADD},
+    {"SUB", 0x658, &builderSUB},
+    {"AND", 0x450, &builderAND},
+    {"ORR", 0x550, &builderORR},
+    {"EOR", 0x650, &builderEOR},
+    {"LSLV", 0x4D6, &builderLSLV},
+    {"LSRV", 0x4D6, &builderLSRV},
 };
 
 const int opcode_table_size = sizeof(opcode_table) / sizeof(OpcodeTable);
